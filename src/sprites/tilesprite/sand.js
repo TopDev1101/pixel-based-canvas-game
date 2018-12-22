8eb9e4ea318b91b9debae63128c5055bfb077774
Daniel Tran <trandap88@gmail.com>
2018-12-22 04:12:40 -0500

    Moved a bunch of files to directories with t3 label

--- src/sprites/tilesprite/sand.js
+++
@@ -1,9 +0,0 @@
-class TileSpriteSand extends TileSpriteNeighbourDependent{
-    constructor( tile, source, atlasKey ){
-        super( tile, source, atlasKey );
-    }
-
-    neighbourCondition( tile ){
-        return tile.identityString == this.tile.identityString || tile.hasIdentity("water");
-    }
-}
--- src/sprites/tilesprite/water.js
+++
@@ -1,6 +0,0 @@
-class TileSpriteWater extends TileSpriteNeighbourDependent{
-    constructor( tile, source, atlasKey ){
-        super( tile, source, atlasKey );
-        this.staticGroundLocation = SSGrounds.getTile("sprite-sand");
-    }
-}
